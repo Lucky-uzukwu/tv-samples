f7f8e3d543dd2ea4527a37df3fd85bc7b838ae7f
luckyuzukwu <uzukwu57@gmail.com>
2025-05-29 00:41:22 +0200

    make search border black

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/search/SearchScreen.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/search/SearchScreen.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2023 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.jetstream.presentation.screens.search
 
 import android.view.KeyEvent
@@ -137,7 +121,7 @@ fun SearchResult(
                         border = BorderStroke(
                             width = if (isTfFocused) 2.dp else 1.dp,
                             color = animateColorAsState(
-                                targetValue = if (isTfFocused) MaterialTheme.colorScheme.primary
+                                targetValue = if (isTfFocused) MaterialTheme.colorScheme.onSurface.copy(alpha = 1f)
                                 else MaterialTheme.colorScheme.border,
                                 label = ""
                             ).value
