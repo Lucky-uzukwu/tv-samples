e19da4eaa03431fbfda11b1c5814e953487f65c0
luckyuzukwu <uzukwu57@gmail.com>
2025-05-29 02:00:33 +0200

    make search screen dark

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/search/SearchScreen.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/presentation/screens/search/SearchScreen.kt
@@ -3,6 +3,7 @@ package com.google.jetstream.presentation.screens.search
 import android.view.KeyEvent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
@@ -28,6 +29,7 @@ import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
@@ -59,7 +61,7 @@ fun SearchScreen(
     val shouldShowTopBar by remember {
         derivedStateOf {
             lazyColumnState.firstVisibleItemIndex == 0 &&
-                lazyColumnState.firstVisibleItemScrollOffset < 100
+                    lazyColumnState.firstVisibleItemScrollOffset < 100
         }
     }
 
@@ -101,107 +103,115 @@ fun SearchResult(
     val tfInteractionSource = remember { MutableInteractionSource() }
 
     val isTfFocused by tfInteractionSource.collectIsFocusedAsState()
-    LazyColumn(
-        modifier = modifier,
-        state = lazyColumnState
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Black)
     ) {
-        item {
-            Surface(
-                shape = ClickableSurfaceDefaults.shape(shape = JetStreamCardShape),
-                scale = ClickableSurfaceDefaults.scale(focusedScale = 1f),
-                colors = ClickableSurfaceDefaults.colors(
-                    containerColor = MaterialTheme.colorScheme.inverseOnSurface,
-                    focusedContainerColor = MaterialTheme.colorScheme.inverseOnSurface,
-                    pressedContainerColor = MaterialTheme.colorScheme.inverseOnSurface,
-                    focusedContentColor = MaterialTheme.colorScheme.onSurface,
-                    pressedContentColor = MaterialTheme.colorScheme.onSurface
-                ),
-                border = ClickableSurfaceDefaults.border(
-                    focusedBorder = Border(
-                        border = BorderStroke(
-                            width = if (isTfFocused) 2.dp else 1.dp,
-                            color = animateColorAsState(
-                                targetValue = if (isTfFocused) MaterialTheme.colorScheme.onSurface.copy(alpha = 1f)
-                                else MaterialTheme.colorScheme.border,
-                                label = ""
-                            ).value
-                        ),
-                        shape = JetStreamCardShape
-                    )
-                ),
-                tonalElevation = 2.dp,
-                modifier = Modifier
-                    .padding(horizontal = childPadding.start)
-                    .padding(top = 8.dp),
-                onClick = { tfFocusRequester.requestFocus() }
-            ) {
-                BasicTextField(
-                    value = searchQuery,
-                    onValueChange = { updatedQuery -> searchQuery = updatedQuery },
-                    decorationBox = {
-                        Box(
-                            modifier = Modifier
-                                .padding(vertical = 16.dp)
-                                .padding(start = 20.dp),
-                        ) {
-                            it()
-                            if (searchQuery.isEmpty()) {
-                                Text(
-                                    modifier = Modifier.graphicsLayer { alpha = 0.6f },
-                                    text = stringResource(R.string.search_screen_et_placeholder),
-                                    style = MaterialTheme.typography.titleSmall
-                                )
-                            }
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            vertical = 4.dp,
-                            horizontal = 8.dp
+        LazyColumn(
+            modifier = modifier,
+            state = lazyColumnState
+        ) {
+            item {
+                Surface(
+                    shape = ClickableSurfaceDefaults.shape(shape = JetStreamCardShape),
+                    scale = ClickableSurfaceDefaults.scale(focusedScale = 1f),
+                    colors = ClickableSurfaceDefaults.colors(
+                        containerColor = MaterialTheme.colorScheme.inverseOnSurface,
+                        focusedContainerColor = MaterialTheme.colorScheme.inverseOnSurface,
+                        pressedContainerColor = MaterialTheme.colorScheme.inverseOnSurface,
+                        focusedContentColor = MaterialTheme.colorScheme.onSurface,
+                        pressedContentColor = MaterialTheme.colorScheme.onSurface
+                    ),
+                    border = ClickableSurfaceDefaults.border(
+                        focusedBorder = Border(
+                            border = BorderStroke(
+                                width = if (isTfFocused) 2.dp else 1.dp,
+                                color = animateColorAsState(
+                                    targetValue = if (isTfFocused) MaterialTheme.colorScheme.onSurface.copy(
+                                        alpha = 1f
+                                    )
+                                    else MaterialTheme.colorScheme.border,
+                                    label = ""
+                                ).value
+                            ),
+                            shape = JetStreamCardShape
                         )
-                        .focusRequester(tfFocusRequester)
-                        .onKeyEvent {
-                            if (it.nativeKeyEvent.action == KeyEvent.ACTION_UP) {
-                                when (it.nativeKeyEvent.keyCode) {
-                                    KeyEvent.KEYCODE_DPAD_DOWN -> {
-                                        focusManager.moveFocus(FocusDirection.Down)
-                                    }
-
-                                    KeyEvent.KEYCODE_DPAD_UP -> {
-                                        focusManager.moveFocus(FocusDirection.Up)
-                                    }
-
-                                    KeyEvent.KEYCODE_BACK -> {
-                                        focusManager.moveFocus(FocusDirection.Exit)
-                                    }
+                    ),
+                    tonalElevation = 2.dp,
+                    modifier = Modifier
+                        .padding(horizontal = childPadding.start)
+                        .padding(top = 8.dp),
+                    onClick = { tfFocusRequester.requestFocus() }
+                ) {
+                    BasicTextField(
+                        value = searchQuery,
+                        onValueChange = { updatedQuery -> searchQuery = updatedQuery },
+                        decorationBox = {
+                            Box(
+                                modifier = Modifier
+                                    .padding(vertical = 16.dp)
+                                    .padding(start = 20.dp),
+                            ) {
+                                it()
+                                if (searchQuery.isEmpty()) {
+                                    Text(
+                                        modifier = Modifier.graphicsLayer { alpha = 0.6f },
+                                        text = stringResource(R.string.search_screen_et_placeholder),
+                                        style = MaterialTheme.typography.titleSmall
+                                    )
                                 }
                             }
-                            true
                         },
-                    cursorBrush = Brush.verticalGradient(
-                        colors = listOf(
-                            LocalContentColor.current,
-                            LocalContentColor.current,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                vertical = 4.dp,
+                                horizontal = 8.dp
+                            )
+                            .focusRequester(tfFocusRequester)
+                            .onKeyEvent {
+                                if (it.nativeKeyEvent.action == KeyEvent.ACTION_UP) {
+                                    when (it.nativeKeyEvent.keyCode) {
+                                        KeyEvent.KEYCODE_DPAD_DOWN -> {
+                                            focusManager.moveFocus(FocusDirection.Down)
+                                        }
+
+                                        KeyEvent.KEYCODE_DPAD_UP -> {
+                                            focusManager.moveFocus(FocusDirection.Up)
+                                        }
+
+                                        KeyEvent.KEYCODE_BACK -> {
+                                            focusManager.moveFocus(FocusDirection.Exit)
+                                        }
+                                    }
+                                }
+                                true
+                            },
+                        cursorBrush = Brush.verticalGradient(
+                            colors = listOf(
+                                LocalContentColor.current,
+                                LocalContentColor.current,
+                            )
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrectEnabled = false,
+                            imeAction = ImeAction.Search
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onSearch = {
+                                searchMovies(searchQuery)
+                            }
+                        ),
+                        maxLines = 1,
+                        interactionSource = tfInteractionSource,
+                        textStyle = MaterialTheme.typography.titleSmall.copy(
+                            color = MaterialTheme.colorScheme.onSurface
                         )
-                    ),
-                    keyboardOptions = KeyboardOptions(
-                        autoCorrectEnabled = false,
-                        imeAction = ImeAction.Search
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onSearch = {
-                            searchMovies(searchQuery)
-                        }
-                    ),
-                    maxLines = 1,
-                    interactionSource = tfInteractionSource,
-                    textStyle = MaterialTheme.typography.titleSmall.copy(
-                        color = MaterialTheme.colorScheme.onSurface
                     )
-                )
+                }
             }
-        }
 // TODO: Uncomment and use
 //        item {
 //            MoviesRow(
@@ -211,5 +221,7 @@ fun SearchResult(
 //                movieList = movieList
 //            ) { selectedMovie -> onMovieClick(selectedMovie) }
 //        }
+        }
+
     }
 }
